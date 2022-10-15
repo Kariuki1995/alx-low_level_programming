6990904856d3e1745f82029e395e5b6d6023fbb9
kenneth kariuki <Kenneth kariuki>
2022-10-15 12:04:13 -0700

    Variables , if , else , while

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - prints in lower and upper case
+ * Return: 0
+ */
+int main(void)
+{
+	char ch;
+
+	for (char = 'a' ; char <= 'z' ; char++)
+	putchar(ch);
+	for (char = 'A' ; char <= 'Z' ; char++)
+	putchar(ch);
+
+	putchar('\n');
+
+	return (0);
+}
