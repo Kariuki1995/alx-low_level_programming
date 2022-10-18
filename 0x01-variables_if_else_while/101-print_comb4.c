08ac9208b4ad2a32e0cafe63878f81b1a6144b62
kenneth kariuki <Kenneth kariuki>
2022-10-18 05:58:48 -0700

    Variables , if else , while

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -10,11 +10,11 @@ int main(void)
 	int j = '0';
 	int k = '0';
 
-	for (i = '0'; i < '9'; i++)
+	for (i = '0'; i <= '9'; i++)
 	{
-		for (j = '0'; j < '9'; j++)
+		for (j = '0'; j <= '9'; j++)
 		{
-			for (k = '0'; k < '9'; k++)
+			for (k = '0'; k <= '9'; k++)
 			{
 				if (!((i == j) || (j == k) || (j > i) || (k > j)))
 				{
