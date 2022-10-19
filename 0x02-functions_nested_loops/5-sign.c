82cacb85252f2607142782b715fb4238a8324046
kenneth kariuki <Kenneth kariuki>
2022-10-19 09:55:29 -0700

    Function , nested loops

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * print_sign - print the number sign
+ * print_sign - prints the number sign
  * @n: is the number to be checked
  * Return: 1 if n is greater than zero,0 if n is zero,-1 if n is less than zero
  */
@@ -11,17 +11,17 @@ int print_sign(int n)
 	if (n > 0)
 	{
 		_putchar('+');
-		return (1);
+		result (1);
 	}
 	else if (n == 0)
 	{
 		_putchar('0');
-		return (0);
+		result 0;
 	}
 	else if (n < 0)
 	{
 		_putchar('-');
-		return (-1);
+		result = -1;
 	}
 	return (result);
 }
