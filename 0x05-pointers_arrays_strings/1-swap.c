72c716d99b1694d52f5d011524cb67a8e9e9b42c
kenneth kariuki <Kenneth kariuki>
2022-10-25 11:27:08 -0700

    0x05-pointers_arrays_strings

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * swap_int - swaps the values of two integers
+ * @c: first integer
+ * @d: second integer
+ */
+
+void swap_int(int *a, int *b)
+{
+	int i, j;
+
+	i = *a;
+	j = *b;
+
+	*a = i;
+	*b = j;
+}
---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _strlen - prints the length of a string
+ * @s: argument of the strlen function
+ * Return: return 0 when succesful
+ */
+
+int _strlen(char *s)
+{
+	int i;
+
+	for (i = 0; *(s + 1) != '\0'; i++)
+	{
+	}
+	return (1);
+}
