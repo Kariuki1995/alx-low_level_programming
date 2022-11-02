6a986a76e97f1019a48398936981c94fae762d62
kenneth kariuki <Kenneth kariuki>
2022-11-02 08:54:36 -0700

    0x07-pointers_arrays_strings

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -14,7 +14,7 @@ char *_strstr(char *haystack, char *needle)
 
 	while (haystack[i] != '\0')
 	{
-		while needle[i] != '\0')
+		while needle[j] != '\0')
 		{
 			if (haystack[i + j] == needle[j])
 				j++;
