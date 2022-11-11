a32f01e36969f2d735234c9fa9f9b64e2576661e
kenneth kariuki <Kenneth kariuki>
2022-11-11 09:09:32 -0800

    0x0C. C - More malloc, free

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * malloc_checked - allocates memory using malloc
+ * @b: the value to be allocated to the memory
+ * Return: returns a pointer to the allocated memory
+ */
+
+void *malloc_checked(unsigned int b)
+{
+	unsigned int *p;
+
+	p = malloc(b);
+
+	if (p == NULL)
+		exit(98);
+	return (p);
+}
---
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,44 @@
+#include "main.h"
+
+/**
+ * string_nconcat - concatenates two strings
+ * @s1: first string
+ * @s2: second string
+ * @n: bytes from s2 to be copied
+ * Return: returns NULL if it fails, otherwise returns pointer P
+ */
+
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+	unsigned int ls1 = 0, ls2 = 0, size, i, j;
+	char *p;
+
+	if (s1 == NULL)
+		s1 = "";
+
+	if (s2 == NULL)
+		s2 = "";
+	
+	while (s1[ls1] != '\0')
+		ls1++;
+
+	while (s2[ls2] != '\0')
+		ls2++;
+
+	if (n >= ls2)
+		n = ls2;
+
+	p = malloc(sizeof(char) * (ls1 + n + 1));
+
+	if (p == NULL)
+		return (NULL);
+
+	for (i = 0; i < ls1; i++)
+		p[i] = s1[i];
+
+	for (j = 0; j < n; j++)
+		p[j + ls1] = s2[j];
+
+	p[i + j] = '\0';
+	return (p);
+}
