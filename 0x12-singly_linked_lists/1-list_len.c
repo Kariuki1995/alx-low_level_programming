635bd0469f0be4b484cc3d42f83c00ff4ed24560
“Kenneth <kenkariuki63@gmail.com>
2022-11-28 10:50:54 -0800

    0x12-singly_linked_lists

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -1,8 +1,11 @@
 #include "lists.h"
+#include <stddef.h>
 
 /**
  * list_len - returns the number of elements in a linked list_t list
  * @h: the structure list_t
+ * @NULL: 0
+ * @next: points to the next node
  * Return: node count
  */
 
